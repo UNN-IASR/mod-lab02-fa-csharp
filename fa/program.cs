24d4b625cc76f4948d17d3f31242e8e6405b7e37
steecH17-Nikita <v.nikita2003@rambler.ru>
2025-03-25 23:57:38 +0300

    add class FA2. The tests are complited

--- fa/program.cs
+++ fa/program.cs
@@ -29,7 +29,6 @@ namespace fans
         public static State q3 = new State("q3", true);
         public static State q4 = new State("q4", false);
         State InitialState = q0;
-
         public FA1()
         {
             q0.Transitions['0'] = q2;
@@ -47,6 +46,7 @@ namespace fans
             q4.Transitions['0'] = q4;
             q4.Transitions['1'] = q4;
         }
+        
         public bool? Run(IEnumerable<char> s)
         {
             
@@ -64,11 +64,41 @@ namespace fans
 
   public class FA2
   {
-    public bool? Run(IEnumerable<char> s)
-    {
-      return false;
+        public static State q1 = new State("q1", false); // оба четные
+        public static State q2 = new State("q2", false); //четное 0 и нечетное 1
+        public static State q3 = new State("q3", false); //нечетное 0 и четное 1
+        public static State q4 = new State("q4", true); //оба нечетные
+        State InitialState = q1;
+
+        public FA2()
+        {
+            q1.Transitions['0'] = q3;
+            q1.Transitions['1'] = q2;
+
+            q2.Transitions['0'] = q4;
+            q2.Transitions['1'] = q1;
+
+            q3.Transitions['0'] = q1;
+            q3.Transitions['1'] = q4;
+
+            q4.Transitions['0'] = q2;
+            q4.Transitions['1'] = q3;
+        }
+
+        public bool? Run(IEnumerable<char> s)
+        {
+
+            State current = InitialState;
+            foreach (var c in s) // цикл по всем символам 
+            {
+                current = current.Transitions[c]; // меняем состояние на то, в которое у нас переход
+                if (current == null)              // если его нет, возвращаем признак ошибки
+                    return null;
+                // иначе переходим к следующему
+            }
+            return current.IsAcceptState;
+        }
     }
-  }
   
   public class FA3
   {
@@ -82,16 +112,16 @@ namespace fans
   {
     static void Main(string[] args)
     {
-      String s = "01111";
-      FA1 fa1 = new FA1();
-      bool? result1 = fa1.Run(s);
-      Console.WriteLine(result1);
-      //FA2 fa2 = new FA2();
-      //bool? result2 = fa2.Run(s);
-      //Console.WriteLine(result2);
-      //FA3 fa3 = new FA3();
-      //bool? result3 = fa3.Run(s);
-      //Console.WriteLine(result3);
-    }
+            String s = "01111";
+            FA1 fa1 = new FA1();
+            bool? result1 = fa1.Run(s);
+            Console.WriteLine(result1);
+            FA2 fa2 = new FA2();
+            bool? result2 = fa2.Run(s);
+            Console.WriteLine(result2);
+            FA3 fa3 = new FA3();
+            bool? result3 = fa3.Run(s);
+            Console.WriteLine(result3);
+        }
   }
 }
