b78f2dd28bdfbf82321b7078ccf2dcd8ff2218dc
serg-1703 <vlasov0210serg@gmail.com>
2025-03-27 20:14:31 +0300

    add FA2

--- fa/program.cs
+++ fa/program.cs
@@ -70,9 +70,55 @@ namespace fans
 
   public class FA2
   {
+    public static State a = new State()
+    {
+      Name = "a",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+    public State b = new State()
+    {
+      Name = "b",
+      IsAcceptState = true,
+      Transitions = new Dictionary<char, State>()
+    };
+    public State c = new State()
+    {
+      Name = "c",
+      IsAcceptState = true,
+      Transitions = new Dictionary<char, State>()
+    };
+    public State d = new State()
+    {
+      Name = "d",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+
+    State InitialState = a;
+        
+    public FA2()
+    {
+      a.Transitions['0'] = b;
+      a.Transitions['1'] = c;
+      b.Transitions['0'] = a;
+      b.Transitions['1'] = d;
+      c.Transitions['0'] = d;
+      c.Transitions['1'] = a;
+      d.Transitions['0'] = c;
+      d.Transitions['1'] = b;
+    }
+        
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State current = InitialState;
+      foreach (var c in s)
+      {
+        current = current.Transitions[c];
+        if (current == null)
+          return null;
+      }
+      return current.IsAcceptState;
     }
   }
   
