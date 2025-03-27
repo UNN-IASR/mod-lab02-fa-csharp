70854360edb7f336ae422b060283fd33751598cf
serg-1703 <vlasov0210serg@gmail.com>
2025-03-27 20:17:24 +0300

    add FA3

--- fa/program.cs
+++ fa/program.cs
@@ -124,9 +124,47 @@ namespace fans
   
   public class FA3
   {
+    public static State a = new State()
+    {
+      Name = "a",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+    public State b = new State()
+    {
+      Name = "b",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+    public State c = new State()
+    {
+      Name = "c",
+      IsAcceptState = true,
+      Transitions = new Dictionary<char, State>()
+    };
+
+    State InitialState = a;
+          
+    public FA3()
+    {
+      a.Transitions['0'] = a;
+      a.Transitions['1'] = b;
+      b.Transitions['0'] = a;
+      b.Transitions['1'] = c;
+      c.Transitions['0'] = c;
+      c.Transitions['1'] = c;
+    }
+        
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State current = InitialState;
+      foreach (var c in s)
+      {
+        current = current.Transitions[c];
+        if (current == null)
+          return null;
+      }
+      return current.IsAcceptState;
     }
   }
 
