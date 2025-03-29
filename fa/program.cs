9f4c1ca2f950849d352140cfad0792270771d3f8
PirnoZiLLa <pirnozilla@gmail.com>
2025-03-30 00:31:42 +0300

    Update program.cs

--- fa/program.cs
+++ fa/program.cs
@@ -8,49 +8,178 @@ namespace fans
 {
   public class State
   {
-    public string Name;
-    public Dictionary<char, State> Transitions;
-    public bool IsAcceptState;
+    public string Label;
+    public Dictionary<char, State> Paths;
+    public bool IsFinalState;
   }
-
 
   public class FA1
   {
-    public bool? Run(IEnumerable<char> s)
-    {
-      return false;
-    }
+        public static State start = new State()
+        {
+            Label = "start",
+            IsFinalState = false,
+            Paths = new Dictionary<char, State>()
+        };
+        public static State mid1 = new State()
+        {
+            Label = "mid1",
+            IsFinalState = false,
+            Paths = new Dictionary<char, State>()
+        };
+        public static State mid2 = new State()
+        {
+            Label = "mid2",
+            IsFinalState = false,
+            Paths = new Dictionary<char, State>()
+        };
+        public static State mid3 = new State()
+        {
+            Label = "mid3",
+            IsFinalState = false,
+            Paths = new Dictionary<char, State>()
+        };
+        public static State accept = new State()
+        {
+            Label = "accept",
+            IsFinalState = true,
+            Paths = new Dictionary<char, State>()
+        };
+
+        State EntryState = start;
+
+        public FA1()
+        {
+            start.Paths['0'] = mid1;
+            start.Paths['1'] = mid2;
+            mid1.Paths['0'] = mid3;
+            mid1.Paths['1'] = accept;
+            mid2.Paths['0'] = accept;
+            mid2.Paths['1'] = mid2;
+            mid3.Paths['0'] = mid3;
+            mid3.Paths['1'] = mid3;
+            accept.Paths['0'] = mid3;
+            accept.Paths['1'] = accept;
+        }
+
+        public bool? Run(IEnumerable<char> input)
+        {
+            State current = EntryState;
+            foreach (var symbol in input)
+            {
+                current = current.Paths[symbol];
+            }
+            return current.IsFinalState;
+        }
   }
 
   public class FA2
   {
-    public bool? Run(IEnumerable<char> s)
-    {
-      return false;
+        public static State start = new State()
+        {
+            Label = "start",
+            IsFinalState = false,
+            Paths = new Dictionary<char, State>()
+        };
+        public static State step1 = new State()
+        {
+            Label = "step1",
+            IsFinalState = false,
+            Paths = new Dictionary<char, State>()
+        };
+        public static State step2 = new State()
+        {
+            Label = "step2",
+            IsFinalState = false,
+            Paths = new Dictionary<char, State>()
+        };
+        public static State success = new State()
+        {
+            Label = "success",
+            IsFinalState = true,
+            Paths = new Dictionary<char, State>()
+        };
+
+        State EntryState = start;
+
+        public FA2()
+        {
+            start.Paths['0'] = step2;
+            start.Paths['1'] = step1;
+            step1.Paths['0'] = success;
+            step1.Paths['1'] = start;
+            step2.Paths['0'] = start;
+            step2.Paths['1'] = success;
+            success.Paths['0'] = step1;
+            success.Paths['1'] = step2;
+        }
+        public bool? Run(IEnumerable<char> input)
+        {
+            State current = EntryState;
+            foreach (var symbol in input)
+            {
+                current = current.Paths[symbol];
+            }
+            return current.IsFinalState;
+        }
     }
-  }
   
   public class FA3
   {
-    public bool? Run(IEnumerable<char> s)
-    {
-      return false;
+        public static State start = new State()
+        {
+            Label = "start",
+            IsFinalState = false,
+            Paths = new Dictionary<char, State>()
+        };
+        public static State middle = new State()
+        {
+            Label = "middle",
+            IsFinalState = false,
+            Paths = new Dictionary<char, State>()
+        };
+        public static State end = new State()
+        {
+            Label = "end",
+            IsFinalState = true,
+            Paths = new Dictionary<char, State>()
+        };
+
+        State EntryState = start;
+
+        public FA3()
+        {
+            start.Paths['0'] = start;
+            start.Paths['1'] = middle;
+            middle.Paths['0'] = start;
+            middle.Paths['1'] = end;
+            end.Paths['0'] = end;
+            end.Paths['1'] = end;
+        }
+        public bool? Run(IEnumerable<char> input)
+        {
+            State current = EntryState;
+            foreach (var symbol in input)
+            {
+                current = current.Paths[symbol];
+            }
+            return current.IsFinalState;
+        }
     }
-  }
 
   class Program
   {
     static void Main(string[] args)
     {
       String s = "01111";
-      FA1 fa1 = new FA1();
-      bool? result1 = fa1.Run(s);
+      FA1 automaton1 = new FA1();
+      bool? result1 = automaton1.Run(s);
       Console.WriteLine(result1);
-      FA2 fa2 = new FA2();
-      bool? result2 = fa2.Run(s);
+      FA2 automaton2 = new FA2();
+      bool? result2 = automaton2.Run(s);
       Console.WriteLine(result2);
-      FA3 fa3 = new FA3();
-      bool? result3 = fa3.Run(s);
+      FA3 automaton3 = new FA3();
+      bool? result3 = automaton3.Run(s);
       Console.WriteLine(result3);
     }
   }
