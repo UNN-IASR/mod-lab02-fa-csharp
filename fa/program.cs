dea344d441bf6f0d81057f9eb31835e43c70951e
Ivan Vladimirov <vivalexy@ya.ru>
2025-03-30 13:42:43 +0300

    Добавлен FA1 и FA2

--- fa/program.cs
+++ fa/program.cs
@@ -8,17 +8,81 @@ namespace fans
 {
   public class State
   {
-    public string Name;
-    public Dictionary<char, State> Transitions;
+    public string? Name;
+    public Dictionary<char, State>? Transitions;
     public bool IsAcceptState;
   }
 
 
   public class FA1
   {
+    State a = new State() // Начальное состояние
+    {
+      Name = "a",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+
+    State b = new State() // Нашли первый ноль
+    {
+      Name = "b",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+
+    State c = new State() // Принимающее состояние
+    {
+      Name = "с",
+      IsAcceptState = true,
+      Transitions = new Dictionary<char, State>()
+    };
+
+    State d = new State() // Отклоняющее состояние (больше одного нуля)
+    {
+      Name = "d",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+
+    State e = new State() // Состояние только для единиц
+    {
+      Name = "e",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+
+    State InitialState;
+
+    public FA1()
+    {
+      InitialState = a;
+
+      a.Transitions['0'] = b;
+      a.Transitions['1'] = e;
+
+      b.Transitions['0'] = d;
+      b.Transitions['1'] = c;
+
+      c.Transitions['0'] = d;
+      c.Transitions['1'] = c;
+
+      d.Transitions['0'] = d;
+      d.Transitions['1'] = d;
+
+      e.Transitions['0'] = c;
+      e.Transitions['1'] = e;
+    }
+
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State current = InitialState;
+      foreach (char c in s)
+      {
+        if (!current.Transitions.ContainsKey(c))
+          return null;
+        current = current.Transitions[c];
+      }
+      return current.IsAcceptState;
     }
   }
 
